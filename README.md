b26ef011cd8e747edf9e68a8ddbb0d04840d020b
tbricca <tbricca@gmail.com>
2018-07-26 08:28:39 -0700

    updated readme, and messed around a little with the css

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-## React To Do List 
+## React To Do List
+
+![Current View Of App](images/ToDos.png)
 
 ## Functionality and Scope of Projet
+
 Users can add to the list and delete items from it. Aimed to create an app to refresh on my React skills and make it look visually appealing.
 
 ## Technologies Used
+
 - React
 - Javascript
 - Node
 - Express
-- Github 
+- Github
 
 ## Screen Shot
-![Current View of App](ScreenshotTodos.jpg)
 
+![First Iteration](images/ScreenshotTodos.jpg)
 
+## Updated View
 
+(images/ToDos.png)
+
+## Next Steps
+
+I will continue to work this to bring additional functionality. I intend on building out multiple lists for users to use because let's be honest, who has just one to do list? I am inspired by Trello and their design. I hope to build something similar to it.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/header';
-import TodoInput from './components/todoInput';
-import TodoItem from './components/todoItem';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/header";
+import TodoInput from "./components/todoInput";
+import TodoItem from "./components/todoItem";
 
 class App extends Component {
   constructor(props) {
@@ -10,12 +10,12 @@ class App extends Component {
 
     this.state = {
       todos: [
-        {id: 0, text: "Go to grocery store"},
-        {id: 1, text: "Do Laundry"},
-        {id: 2, text: "Do a new coding project"}
+        { id: 0, text: "Go to grocery store" },
+        { id: 1, text: "Do Laundry" },
+        { id: 2, text: "Do a new coding project" }
       ],
       nextId: 3
-    }
+    };
 
     this.addTodo = this.addTodo.bind(this);
     this.removeTodo = this.removeTodo.bind(this);
@@ -23,33 +23,54 @@ class App extends Component {
 
   addTodo(todoText) {
     let todos = this.state.todos.slice();
-    todos.push({id: this.state.nextId, text: todoText});
+    todos.push({ id: this.state.nextId, text: todoText });
     this.setState({
       todos: todos,
       nextId: ++this.state.nextId
-    })
+    });
   }
 
   removeTodo(id) {
     this.setState({
       todos: this.state.todos.filter((todo, index) => todo.id !== id)
-    })
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <div className="todo-wrapper">
-         <Header />
-         <TodoInput todoText="" addTodo={this.addTodo}/>
-         <ul>
-           {
-             this.state.todos.map((todo) => {
-               return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo} />
-             })
-           }
-         </ul>
+        <div className="todo-wrapper wrap1 container">
+          <Header />
+          <TodoInput todoText="" addTodo={this.addTodo} />
+          <ul>
+            {this.state.todos.map(todo => {
+              return (
+                <TodoItem
+                  todo={todo}
+                  key={todo.id}
+                  id={todo.id}
+                  removeTodo={this.removeTodo}
+                />
+              );
+            })}
+          </ul>
         </div>
+        {/* <div className="todo-wrapper wrap2 container">
+          <Header />
+          <TodoInput todoText="" addTodo={this.addTodo} />
+          <ul>
+            {this.state.todos.map(todo => {
+              return (
+                <TodoItem
+                  todo={todo}
+                  key={todo.id}
+                  id={todo.id}
+                  removeTodo={this.removeTodo}
+                />
+              );
+            })}
+          </ul>
+        </div> */}
       </div>
     );
   }
